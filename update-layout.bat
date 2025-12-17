e0f6b6829f64f5adf014c7d96cd6579bf4183b5c
정문주 <>
2025-12-17 09:07:07 +0900

    1217-b update-layout.bat

--- update-layout.bat
+++ update-layout.bat
@@ -15,9 +15,11 @@ echo ==================================================
 
 if not exist "assets\css" mkdir "assets\css"
 if not exist "assets\js" mkdir "assets\js"
+if not exist "assets\img" mkdir "assets\img"
 
 :: 'assets' 폴더 가져오기 (덮어씀)
 @REM xcopy "dodram-all\assets" "assets" /E /H /Y /I
+xcopy "dodram-all\assets\img\layout" "assets\img\layout" /E /H /Y /I
 
 copy /Y "dodram-all\template.html" .
 copy /Y "dodram-all\assets\css\layout.css" "assets\css\"
